b3f8e6467c24ca7655ef1fe896915f03f55f8fc2
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-02-27 21:28:57 +0000

    Basic CSS and JS implementation

---
+++ javascript/content.js
@@ -0,0 +1,10 @@
+const flashcard = {
+  questions: "How do you install gems you added to your Gemfile?",
+  answer: "Kill any web server running (Ctrl + C). Run bundle install. Restart the Rails server with rails s"
+}
+
+console.log(flashcard)
+
+// fetch("https://api.github.com/users/ssaunier")
+//   .then(response => response.json())
+//   .then(data => console.log(data))
